908fb4d773a063edaedd06167779b77d2eb2e82f
朱波 <956903326@qq.com>
2025-02-20 10:10:37 +0800

    feat: corejs

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import fs from 'fs';
 import babel from "@babel/core";
+import presetEnv from '@babel/preset-env';
 
 const code = fs.readFileSync("./test.js", "utf8");
 
 const result = babel.transform(code, {
-    presets: ['@babel/preset-env'],
+    presets: [
+        //usage 会根据配置的浏览器兼容，以及你代码中用到的 API 来进行 polyfill，实现了按需添加
+        //corejs 3 是 corejs 的版本
+        [presetEnv, {useBuiltIns: "usage", corejs: 3}]
+    ],
     plugins: []
 });
 
--- result.js
+++ result.js
@@ -1,6 +1,20 @@
 "use strict";
 
+require("core-js/modules/es.symbol.js");
+require("core-js/modules/es.symbol.description.js");
+require("core-js/modules/es.symbol.iterator.js");
+require("core-js/modules/es.symbol.to-primitive.js");
+require("core-js/modules/es.array.iterator.js");
+require("core-js/modules/es.date.to-primitive.js");
+require("core-js/modules/es.number.constructor.js");
+require("core-js/modules/es.object.define-property.js");
+require("core-js/modules/es.string.iterator.js");
+require("core-js/modules/web.dom-collections.iterator.js");
 function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+require("core-js/modules/es.array.concat.js");
+require("core-js/modules/es.array.filter.js");
+require("core-js/modules/es.object.assign.js");
+require("core-js/modules/es.object.to-string.js");
 function _defineProperties(e, r) { for (var t = 0; t < r.length; t++) { var o = r[t]; o.enumerable = o.enumerable || !1, o.configurable = !0, "value" in o && (o.writable = !0), Object.defineProperty(e, _toPropertyKey(o.key), o); } }
 function _createClass(e, r, t) { return r && _defineProperties(e.prototype, r), t && _defineProperties(e, t), Object.defineProperty(e, "prototype", { writable: !1 }), e; }
 function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
